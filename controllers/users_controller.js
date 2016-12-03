be37a2ee2cd73c687c1a19426dfd4c83569f2e72
clarksandholtz <clarksandholtz@gmail.com>
2016-12-03 10:55:40 -0700

    Testing add to map on home page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,14 +3,17 @@ var googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyDtWkCxBMVrziS_hxagImZumHnMvTsfZcw'
 });
 var mongoose = require('mongoose'),
-    User = mongoose.model('User');
-function hashPW(pwd){
+  User = mongoose.model('User');
+
+function hashPW(pwd) {
   return crypto.createHash('sha256').update(pwd).
-         digest('base64').toString();
+  digest('base64').toString();
 }
-exports.signup = function(req, res){
+exports.signup = function(req, res) {
   console.log("Begin exports.signup");
-  var user = new User({username:req.body.username});
+  var user = new User({
+    username: req.body.username
+  });
   console.log("after new user exports.signup");
   user.set('hashed_password', hashPW(req.body.password));
   console.log("after hashing user exports.signup");
@@ -19,7 +22,7 @@ exports.signup = function(req, res){
   user.save(function(err) {
     console.log("In exports.signup");
     console.log(err);
-    if (err){
+    if (err) {
       res.session.error = err;
       res.redirect('/signup');
     } else {
@@ -30,100 +33,110 @@ exports.signup = function(req, res){
     }
   });
 };
-exports.login = function(req, res){
-  User.findOne({ username: req.body.username })
-  .exec(function(err, user) {
-    if (!user){
-      err = 'User Not Found.';
-    } else if (user.hashed_password === 
-               hashPW(req.body.password.toString())) {
-      req.session.regenerate(function(){
-        console.log("login");
-        console.log(user);
-        req.session.user = user.id;
-        req.session.username = user.username;
-        req.session.msg = 'Authenticated as ' + user.username;
-        req.session.locations = user.locations;
-        res.redirect('/');
-      });
-    }else{
-      err = 'Authentication failed.';
-    }
-    if(err){
-      req.session.regenerate(function(){
-        req.session.msg = err;
-        res.redirect('/login');
-      });
-    }
-  });
-};
-exports.getUserProfile = function(req, res) {
-  User.findOne({ _id: req.session.user })
-  .exec(function(err, user) {
-    if (!user){
-      res.json(404, {err: 'User Not Found.'});
-    } else {
-      res.json(user);
-    }
-  });
-};
-exports.updateUser = function(req, res){
-  User.findOne({ _id: req.session.user })
-  .exec(function(err, user) {
-    user.set('email', req.body.email);
-    var passedLoc = (req.body.locations);
-
-    var myLat, myLng, myLoc;
-
-    googleMapsClient.geocode({
-      address: req.body.locations
-    }, function(err, response) {
-      if (!err) {
-        console.log(response.json.results[0].geometry.location);
-	myLat = response.json.results[0].geometry.location.lat;
-	myLng = response.json.results[0].geometry.location.lng;
-	console.log("Latitude: " + myLat + " Longitude: " + myLng);
-
-    myLoc = {
-      name: passedLoc,
-      lat: myLat,
-      long: myLng,
-      comments: [],
-      photosURL:""
-    };
-    
-    console.log(myLoc);
-   user.locations.push(myLoc);
-    user.save(function(err) {
-      if (err){
-        res.sessor.error = err;
+exports.login = function(req, res) {
+  User.findOne({
+      username: req.body.username
+    })
+    .exec(function(err, user) {
+      if (!user) {
+        err = 'User Not Found.';
+      } else if (user.hashed_password ===
+        hashPW(req.body.password.toString())) {
+        req.session.regenerate(function() {
+          console.log("login");
+          console.log(user);
+          req.session.user = user.id;
+          req.session.username = user.username;
+          req.session.msg = 'Authenticated as ' + user.username;
+          req.session.locations = user.locations;
+          res.redirect('/');
+        });
       } else {
-        req.session.msg = 'User Updated.';
-        req.session.locations = req.body.locations;
+        err = 'Authentication failed.';
       }
-      res.redirect('/user');
-    });
-      }
-    });
-  });
-};
-exports.deleteUser = function(req, res){
-  User.findOne({ _id: req.session.user })
-  .exec(function(err, user) {
-    if(user){
-      user.remove(function(err){
-        if (err){
+      if (err) {
+        req.session.regenerate(function() {
           req.session.msg = err;
-        }
-        req.session.destroy(function(){
           res.redirect('/login');
         });
+      }
+    });
+};
+exports.getUserProfile = function(req, res) {
+  User.findOne({
+      _id: req.session.user
+    })
+    .exec(function(err, user) {
+      if (!user) {
+        res.json(404, {
+          err: 'User Not Found.'
+        });
+      } else {
+        res.json(user);
+      }
+    });
+};
+exports.updateUser = function(req, res) {
+  User.findOne({
+      _id: req.session.user
+    })
+    .exec(function(err, user) {
+      user.set('email', req.body.email);
+      var passedLoc = (req.body.location);
+
+      var myLat, myLng, myLoc;
+
+      googleMapsClient.geocode({
+        address: passedLoc
+      }, function(err, response) {
+        if (!err) {
+          console.log(response.json.results[0].geometry.location);
+          myLat = response.json.results[0].geometry.location.lat;
+          myLng = response.json.results[0].geometry.location.lng;
+          console.log("Latitude: " + myLat + " Longitude: " + myLng);
+
+          myLoc = {
+            name: passedLoc,
+            lat: myLat,
+            long: myLng,
+            comments: [],
+            photosURL: ""
+          };
+
+          console.log(myLoc);
+          user.locations.push(myLoc);
+          user.save(function(err) {
+            if (err) {
+              res.sessor.error = err;
+            } else {
+              req.session.msg = 'User Updated.';
+              req.session.locations = user.locations;
+            }
+            res.redirect('/');
+          });
+        }
       });
-    } else{
-      req.session.msg = "User Not Found!";
-      req.session.destroy(function(){
-        res.redirect('/login');
-      });
-    }
-  });
+    });
+};
+exports.deleteUser = function(req, res) {
+  User.findOne({
+      _id: req.session.user
+    })
+    .exec(function(err, user) {
+      if (user) {
+        user.remove(function(err) {
+          if (err) {
+            req.session.msg = err;
+          }
+          req.session.destroy(function() {
+            res.redirect('/login');
+          });
+        });
+      } else {
+        req.session.msg = "User Not Found!";
+        req.session.destroy(function() {
+          res.redirect('/login');
+        });
+      }
+    });
 };
